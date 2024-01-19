6286d3d3f2eb0b5c6b07f15a8a5e0ddaad95be0e
James1412 <73318218+James1412@users.noreply.github.com>
2024-01-19 15:28:27 -0500

    scale images with valuenotifier

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -13,9 +13,20 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   final PageController _pageController = PageController(viewportFraction: 0.7);
   int _currentPage = 0;
 
+  final ValueNotifier<double> _scroll = ValueNotifier(0.0);
+
   void _onPageChanged(int newPage) {
     setState(() {
       _currentPage = newPage;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      if (_pageController.page == null) return;
+      _scroll.value = _pageController.page!;
     });
   }
 
@@ -59,23 +70,33 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
               return Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    height: 350,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.4),
-                          blurRadius: 10,
-                          spreadRadius: 2,
-                          offset: const Offset(0, 8),
+                  ValueListenableBuilder(
+                    valueListenable: _scroll,
+                    builder: (context, scroll, child) {
+                      final difference = (scroll - index).abs();
+                      final scale = 1 - (difference * 0.1);
+                      return Transform.scale(
+                        scale: scale,
+                        child: Container(
+                          height: 350,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.4),
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                                offset: const Offset(0, 8),
+                              ),
+                            ],
+                            image: DecorationImage(
+                              image: AssetImage('assets/${index + 1}.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
-                      ],
-                      image: DecorationImage(
-                        image: AssetImage('assets/${index + 1}.png'),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+                      );
+                    },
                   ),
                   const SizedBox(
                     height: 35,
