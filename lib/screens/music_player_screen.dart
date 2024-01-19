9f459916ae230945c49c34dc8f4df254dfc61d2a
James1412 <73318218+James1412@users.noreply.github.com>
2024-01-19 03:51:23 -0500

    animatedswiter and backdropfilter widgets

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -9,13 +11,47 @@ class MusicPlayerScreen extends StatefulWidget {
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
   final PageController _pageController = PageController(viewportFraction: 0.7);
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 500),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/${_currentPage + 1}.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 20,
+                  sigmaY: 20,
+                ),
+                child: Container(color: Colors.black.withOpacity(0.5)),
+              ),
+            ),
+          ),
           PageView.builder(
+            onPageChanged: _onPageChanged,
             controller: _pageController,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
@@ -26,8 +62,18 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                   Container(
                     height: 350,
                     decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.4),
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: const Offset(0, 8),
+                        ),
+                      ],
                       image: DecorationImage(
                         image: AssetImage('assets/${index + 1}.png'),
+                        fit: BoxFit.cover,
                       ),
                     ),
                   ),
@@ -36,14 +82,14 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                   ),
                   const Text(
                     "5cm/second",
-                    style: TextStyle(fontSize: 26),
+                    style: TextStyle(fontSize: 26, color: Colors.white),
                   ),
                   const SizedBox(
                     height: 10,
                   ),
                   const Text(
                     "Shinkai Makoto",
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ],
               );
